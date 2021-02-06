5e9c88645c5a480b44864c422fe4ebfd899e620c
Caleigh Ertel <ertelc@usf.edu>
2021-02-06 16:34:54 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //user will input their name 
+            string name;
+            Console.WriteLine("Please enter your name.");
+            //assigning the user's input
+            name = Console.ReadLine();
+            //I included some page breaks for easier readibility 
+            Console.WriteLine(" ");
+            Console.WriteLine("__________________________________");
+            Console.WriteLine(" ");
+            
+            //user is greeted with whatever name they inputted
+
+            Console.WriteLine("Hello {0}!", name);
+        }
+    }
+}
